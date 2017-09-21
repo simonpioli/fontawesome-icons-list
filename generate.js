9f1f203dd96acafbc15c74b59e49bbd68e125faf
Simon Pioli <simon@simonpioli.com>
2017-09-21 17:40:46 +0100

    Adapt for use when isn’t parent module

--- generate.js
+++ generate.js
@@ -1,18 +1,19 @@
 var fs   = require('fs');
-var icons = require('./icons');
+var path = require('path');
+var icons = require(__dirname + '/icons');
 
-fs.writeFileSync('icons.json', JSON.stringify(icons));
+fs.writeFileSync(path.join(__dirname, 'icons.json'), JSON.stringify(icons));
 
-fs.writeFileSync('index.js', ['(function (root, factory) {',
-'  if(typeof define === "function" && define.amd) {',
-'    define([], function(){',
-'      return (root.fontawesomeIconsList = factory());',
-'    });',
-'  } else if(typeof module === "object" && module.exports) {',
-'    module.exports = (root.fontawesomeIconsList = factory());',
-'  } else {',
-'    root.fontawesomeIconsList = factory();',
-'  }',
-'}(this, function() {',
-'  return ' + JSON.stringify(icons) + ';',
-'}));'].join('\n'));
+fs.writeFileSync(path.join(__dirname, 'index.js'), ['(function (root, factory) {',
+    '  if(typeof define === "function" && define.amd) {',
+    '    define([], function(){',
+    '      return (root.fontawesomeIconsList = factory());',
+    '    });',
+    '  } else if(typeof module === "object" && module.exports) {',
+    '    module.exports = (root.fontawesomeIconsList = factory());',
+    '  } else {',
+    '    root.fontawesomeIconsList = factory();',
+    '  }',
+    '}(this, function() {',
+    '  return ' + JSON.stringify(icons) + ';',
+    '}));'].join('\n'));
--- icons.js
+++ icons.js
@@ -2,50 +2,48 @@ var css  = require('css');
 var fs   = require('fs');
 var path = require('path');
 
-var findup = require('findup-sync');
-
-var fontAwesomePath = findup('font-awesome.css');
-if (fontAwesomePath === null) {
+var fontAwesomePath = path.join(__dirname, '../', 'font-awesome', 'css', 'font-awesome.css');
+if (fs.existsSync(fontAwesomePath) === false) {
     fontAwesomePath = path.join(__dirname, 'node_modules', 'font-awesome', 'css', 'font-awesome.css');
 }
 var fontAwesomeCSS = fs.readFileSync(fontAwesomePath, 'utf8');
 
 function isFASelector(selector) {
-  return selector.indexOf('.fa') === 0;
+    return selector.indexOf('.fa') === 0;
 }
 
 function hasFASelector(obj) {
-  for (var i in obj.selectors) {
-    if (isFASelector(obj.selectors[i])) {
-      return true;
+    for (var i in obj.selectors) {
+        if (isFASelector(obj.selectors[i])) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 function isFAIcon(obj) {
-  for (var i in obj.declarations) {
-    var declaration = obj.declarations[i];
-    if (declaration.type === 'declaration' && declaration.property === 'content') {
-      return true;
+    for (var i in obj.declarations) {
+        var declaration = obj.declarations[i];
+        if (declaration.type === 'declaration' && declaration.property === 'content') {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 function isIcon (obj) {
-  return obj.type === 'rule' && hasFASelector(obj) && isFAIcon(obj);
+    return obj.type === 'rule' && hasFASelector(obj) && isFAIcon(obj);
 }
 
 
 var fontAwesome = css.parse(fontAwesomeCSS).stylesheet.rules;
 var icons = [];
 fontAwesome.filter(function (obj) {
-  return isIcon(obj);
+    return isIcon(obj);
 }).forEach(function (obj) {
-  obj.selectors.forEach(function (selector) {
-    icons.push(selector.split(':')[0].substring(1));
-  });
+    obj.selectors.forEach(function (selector) {
+        icons.push(selector.split(':')[0].substring(1));
+    });
 });
 
 module.exports = icons;
